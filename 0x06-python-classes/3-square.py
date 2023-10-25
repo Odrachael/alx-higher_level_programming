d9fafc8d44f4c77cbe6583ff9dc375c5ce2f791b
Odrachael <missrachael41@gmail.com>
2023-10-25 20:15:38 +0100

    task 3 corrected

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,3 +15,11 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
